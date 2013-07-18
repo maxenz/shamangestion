fa38142c0fb03c32112be6e95e6b92f3c4066360
Maximiliano Poggio <maximilianopoggio@gmail.com>
2013-07-18 17:01:19 -0300

    arregle orden visor de logs, agregue campo serial a vista de licencias_cliente

--- app/assets/javascripts/clientes.js
+++ app/assets/javascripts/clientes.js
@@ -20,21 +20,21 @@ $(document).ready(function() {
 
 
 
-tClienteLicencias = $('#cliente_licencias').dataTable({
-    "fnDrawCallback": function(){
-      hoverBackgroundTable();
-    },
-    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-  	"sPaginationType": "bootstrap",
-    "iDisplayLength": 8,
-    "aLengthMenu": [[8, 10, 15, -1], [8, 10, 15, "All"]],
-     "aoColumnDefs": [
-	      { "sWidth": "5%", "aTargets": [ 4,5 ] }
-	    ],
-    "bSortClasses": false,
-  	"oLanguage": {
-     	"sSearch" : "Buscar:"
-    }
+	tClienteLicencias = $('#cliente_licencias').dataTable({
+	    "fnDrawCallback": function(){
+	      hoverBackgroundTable();
+	    },
+	    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+	  	"sPaginationType": "bootstrap",
+	    "iDisplayLength": 8,
+	    "aLengthMenu": [[8, 10, 15, -1], [8, 10, 15, "All"]],
+	     "aoColumnDefs": [
+		      { "sWidth": "5%", "aTargets": [ 5,6 ] }
+		    ],
+	    "bSortClasses": false,
+	  	"oLanguage": {
+	     	"sSearch" : "Buscar:"
+	    }
 	});
 
 
--- app/controllers/licencias_logs_controller.rb
+++ app/controllers/licencias_logs_controller.rb
@@ -3,7 +3,7 @@ class LicenciasLogsController < ApplicationController
 before_filter :authenticate_user!
 
 def index
-    @licencias_logs = LicenciasLog.all
+    @licencias_logs = LicenciasLog.order("created_at DESC").all
 
     respond_to do |format|
       format.html # index.html.erb
