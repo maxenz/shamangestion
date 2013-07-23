0b6b58a8fa56de363360b958573c260d0bd8429d
Maximiliano Poggio <maximilianopoggio@gmail.com>
2013-07-23 17:14:22 -0300

    Resuelto el problema de visor de logs cuando se borra un cliente_licencia

--- app/helpers/licencias_logs_helper.rb
+++ app/helpers/licencias_logs_helper.rb
@@ -1,2 +1,12 @@
 module LicenciasLogsHelper
+	def get_cliente_if_exists log
+
+		cliente = log.licencia.cliente
+		if cliente.nil?
+			return "CLIENTE SIN LICENCIA"
+		else
+			return cliente.razonSocial
+		end
+
+	end
 end
