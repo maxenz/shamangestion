f384e762ff072c6c3c53a3bdd30a95b9aff4459d
Maximiliano Poggio <maximilianopoggio@gmail.com>
2013-10-18 11:05:35 -0300

    resuelto bug log

--- app/helpers/licencias_logs_helper.rb
+++ app/helpers/licencias_logs_helper.rb
@@ -1,11 +1,21 @@
 module LicenciasLogsHelper
 	def get_cliente_if_exists log
 
-		cliente = log.licencia.cliente
-		if cliente.nil?
+		licencia = log.licencia
+		if licencia.nil?
 			return "CLIENTE SIN LICENCIA"
 		else
-			return cliente.razonSocial
+			return licencia.cliente.razonSocial
 		end
+	end
+	
+		def get_licencia_if_exists log
+	
+			licencia = log.licencia
+			if licencia.nil?
+				return "CLIENTE SIN LICENCIA"
+			else
+				return licencia.serial
+			end
 	end
 end
